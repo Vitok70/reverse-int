77feb8e88d5245113a3f53f42c832ce0ecf32a65
Vitok70 <vitokunev70@gmail.com>
2022-08-01 20:00:44 +0300

    feat:silution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let str = '';
+  let rez;
+  let a = (n < 0) ? n *(- 1) : n;
+  str = a.toString(10).split('').reverse().join('');
+
+  for (i=0; i < str.length; i++){
+     if (str.indexOf('0') == 0){
+         str = str.slice(1);
+     }
+  }
+ rez = Number(str);
+  return rez;
 }
